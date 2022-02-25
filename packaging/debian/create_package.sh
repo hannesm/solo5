c6503f54e511650cc07a6f1506c104e816904e69
Robur <team@robur.coop>
2022-02-25 11:26:00 +0000

    Debian packaging: set architecture to DEB_TARGET_ARCH

--- packaging/debian/create_package.sh
+++ packaging/debian/create_package.sh
@@ -26,6 +26,9 @@ install -m 0644 $basedir/packaging/debian/control $debiandir/control
 install -m 0644 $basedir/packaging/debian/changelog $debiandir/changelog
 install -m 0644 $basedir/packaging/debian/copyright $debiandir/copyright
 
+ARCH=$(dpkg-architecture -q DEB_TARGET_ARCH)
+sed -i -e "s/^Architecture:.*/Architecture: ${ARCH}/" $debiandir/control
+
 dpkg-deb --build $rootdir $basedir/solo5-hvt.deb
 echo 'bin: [ "solo5-hvt.deb" ]' > $basedir/solo5-bindings-hvt.install
 echo 'doc: [ "README.md" ]' >> $basedir/solo5-bindings-hvt.install
